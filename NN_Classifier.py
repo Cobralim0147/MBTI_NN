960f2e0f89a8a15a6d8f8a38cfbc0afcb5b28ad8
Cobra <cobra19008085@gmail.com>
2024-12-15 16:52:56 +0800

    changed the value of max_iter

--- NN_Classifier.py
+++ NN_Classifier.py
@@ -82,7 +82,7 @@ X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_scaled, test_siz
 
 # mlp = MLPClassifier(hidden_layer_sizes=(5), max_iter=50)
 mlp = MLPClassifier(
-    hidden_layer_sizes=(20, 20, 20), max_iter=50, activation='tanh', solver='lbfgs', random_state=42)
+    hidden_layer_sizes=(20, 20, 20), max_iter=200, activation='tanh', solver='lbfgs', random_state=42)
 
 mlp.fit(X_train, y_train)
 predictions = mlp.predict(X_test)
